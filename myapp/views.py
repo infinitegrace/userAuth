3a899204e4b08a7d0690b83b41650d5ba9d3e15e
Uche <uchegodswill480@gmail.com>
2024-07-03 21:39:09 +0100

    second commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from . models import Feature
 # Create your views here.
 def index(request):
-  return render(request, 'index.html')
+  feature1 = Feature()
+  feature1.id = 0
+  feature1.name = 'franklin'
+  feature1.details = "Has been tested and trusted "
+  feature1.is_true = True
+  
+  feature2 = Feature()
+  feature2.id = 1
+  feature2.name = 'Uche'
+  feature2.details = 'Very afordable'
+  feature2.is_true = False
+  
+  features = [feature1,feature2]
+  return render(request, 'index.html',{'feature':features})
