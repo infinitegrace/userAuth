1f12494dde6f97daf37ed6ac8cb7fcd1eef8cb6b
Uche Godswill O <82162216+infinitegrace@users.noreply.github.com>
2024-07-08 17:56:27 +0100

    models added from data bsae

--- myapp/views.py
+++ myapp/views.py
@@ -2,19 +2,8 @@ from django.shortcuts import render
 from . models import Feature
 # Create your views here.
 def index(request):
-  feature1 = Feature()
-  feature1.id = 0
-  feature1.name = 'franklin'
-  feature1.details = "Has been tested and trusted "
-  feature1.is_true = True
   
-  feature2 = Feature()
-  feature2.id = 1
-  feature2.name = 'Uche'
-  feature2.details = 'Very afordable'
-  feature2.is_true = False
-  
-  features = [feature1,feature2]
+  features = Feature.objects.all
   return render(request, 'index.html',{'feature':features})
 
 def contact(request):
