0d98377b3b271fdfaa8d4488f0e0378d25316078
Uche Godswill O <82162216+infinitegrace@users.noreply.github.com>
2024-07-08 17:25:04 +0100

    models added

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . models import Feature, Attributes
 # Register your models here.
+
+admin.site.register(Feature)
+admin.site.register(Attributes)
--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Feature(models.Model):
   name = models.CharField(max_length = 100)
   details = models.CharField(max_length = 500)
+
+class Attributes(models.Model):
+  designer = models.CharField(max_length = 50)
+  Age = models.CharField(max_length = 100)
